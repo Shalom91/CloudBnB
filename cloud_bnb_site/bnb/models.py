c117a5f1a567ce4bde841fc8a1f4dcb7904b9769
Shalom91 <shalom.mothapo@gmail.com>
2020-10-05 16:02:48 +0200

    created Property model

--- cloud_bnb_site/bnb/models.py
+++ cloud_bnb_site/bnb/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Property(models.Model):
+    description_text = models.CharField(max_length=200)
+    property_pictures = models.ImageField()
+    city = models.CharField(max_length=50)
+    suburb = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=50)
+    max_number_of_guest = models.IntegerField()
+    price_per_night_one_guest = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    
